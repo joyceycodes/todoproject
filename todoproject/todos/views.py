28af7ee14419034e79db5acd00ad260156bac3d7
Joyce Yu <joyceyu96@gmail.com>
2023-06-26 22:35:31 -0700

    added delete todo item view function

--- todoproject/todos/views.py
+++ todoproject/todos/views.py
@@ -5,10 +5,18 @@ from .models import Todo
 # Create your views here.
 
 def list_todo_items(request):
-    return render(request,'todos/todo_list.html')
+    context = { 'todo_list' : Todo.objects.all() }
+
+    return render(request,'todos/todo_list.html', context)
 
 def insert_todo_item(request:HttpRequest):
-    # django ORM will handle the queries for us    
+    # django ORM will handle the queries for us   
+    # creating a Todo object with the content from our input button 
     todo = Todo(content = request.POST['content'])
     todo.save()
+    return redirect('/todos/list/')
+
+def delete_todo_item(request, todo_id):
+    todo_to_delete = Todo.objects.get(id=todo_id)
+    todo_to_delete.delete()
     return redirect('/todos/list/')
