02be5dabdcea2053ea80c851d3d688a2d5f8940b
Joyce Yu <joyceyu96@gmail.com>
2023-06-26 22:13:44 -0700

    added insert todo item view function

--- todoproject/todos/views.py
+++ todoproject/todos/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, HttpRequest
+from .models import Todo
 
 # Create your views here.
 
 def list_todo_items(request):
     return render(request,'todos/todo_list.html')
+
+def insert_todo_item(request:HttpRequest):
+    # django ORM will handle the queries for us    
+    todo = Todo(content = request.POST['content'])
+    todo.save()
+    return redirect('/todos/list/')
